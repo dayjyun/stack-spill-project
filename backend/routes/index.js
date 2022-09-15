8036b228f8fe84ebb3c8e8c8fbc85003f2e94524
kevin <kevinbarrios@ymail.com>
2022-09-15 04:46:27 -0500

    current updates with folders. Complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -29,19 +29,19 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
-  });
-}
-
-// router.get("/api/csrf/restore", (req, res) => {
-//   const csrfToken = req.csrfToken();
-//   res.cookie("XSRF-TOKEN", csrfToken);
-//   res.status(200).json({
-//     "XSRF-Token": csrfToken
+// if (process.env.NODE_ENV !== 'production') {
+//   router.get('/api/csrf/restore', (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.json({});
 //   });
-// });
+// }
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken
+  });
+});
 
 module.exports = router;
