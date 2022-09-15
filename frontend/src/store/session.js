eb0508f4ff7aa239c3347b269488aed5bef2f67b
kevin <kevinbarrios@ymail.com>
2022-09-15 02:15:11 -0500

    completed phase 3

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -61,6 +61,14 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 // reducers
 const initialState = { user: null };
 
