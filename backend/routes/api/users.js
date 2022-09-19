c07ef756e0878dedead5bffa093c0687568e58fd
kevin <kevinbarrios@ymail.com>
2022-09-18 23:24:47 -0500

    current updates

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,42 +3,32 @@ const express = require("express");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const router = express.Router();
-const { check } = require("express-validator");
-const { handleValidationErrors } = require("../../utils/validation");
-
-const validateSignup = [
-  check("firstName")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide your first name"),
-  check("lastName")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide your last name"),
-  check("email")
-    .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage("Please provide a valid email."),
-  check("username")
-    .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage("Please provide a username with at least 4 characters."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
-  check("password")
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage("Password must be 6 characters or more."),
-  handleValidationErrors
-];
+const { validateSignup } = require("../../utils/validation");
 
 //================== Sign up ==========================//
 router.post("/signup", validateSignup, async (req, res) => {
   const { firstName, lastName, email, password, username } = req.body;
   const user = await User.signup({ firstName, lastName, email, username, password }); // removed profileImage
+  const checkEmail = await User.findOne({ where: { email }})
+  const checkUsername = await User.findOne({ where: { username } })
+
+  if (checkEmail) {
+    let error = new Error("User already exists")
+    error.status = 403;
+    error.errors = ['User with that email already exists'];
+    throw error;
+  }
+
+  if(checkUsername){
+    let error = new Error("User already exists")
+    error.status = 403;
+    error.errors = ["User with that username already exists"];
+    throw error;
+  }
 
   await setTokenCookie(res, user);
 
-  return res.json({
-    user
-  });
+  return res.json({ user });
 });
 
 // test route
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const { check } = require("express-validator");
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
@@ -17,6 +18,31 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
+// api/users.js
+const validateSignup = [
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide your first name"),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide your last name"),
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("username")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
+];
+
 module.exports = {
-  handleValidationErrors
+  handleValidationErrors,
+  validateSignup,
 };
