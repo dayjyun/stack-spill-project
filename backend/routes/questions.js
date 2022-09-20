e1235871d3b92e5b304ee5ca459e2f70bb17520c
kevin <kevinbarrios@ymail.com>
2022-09-20 17:16:59 -0500

    completed create a question route and ordered all questions query desc

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,37 +1,45 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const { Question } = require("../db/models");
+const { requireAuth } = require("../utils/auth");
 
 // Get Question from an ID
-router.get('/:questionId', async (req, res) => {
-    const { questionId } = req.params;
-    const Query = await Question.findByPk(questionId)
+router.get("/:questionId", async (req, res) => {
+  const { questionId } = req.params;
+  const Query = await Question.findByPk(questionId);
 
-    if (!Query) {
-        
-    }
-
-    res.json({ Query })
-})
-
+  if (!Query) {
+    const error = new Error("Question not found");
+    error.status = 404;
+    throw error;
+  }
+  res.json({ Query });
+});
 
 // Get All Questions
-router.get('/', async (req, res) => {
-    const Questions = await Question.findAll()
-    res.json({ Questions })
-})
-
+router.get("/", async (req, res) => {
+  const Questions = await Question.findAll({
+    order: [['createdAt', 'DESC']]
+  });
+  res.json({ Questions });
+});
 
 // Create A Question
+router.post("/", requireAuth, async (req, res) => {
+  const { user } = req;
+  const { title, body } = req.body;
+  const question = await Question.create({
+    userId: user.id,
+    title,
+    body,
+  });
 
-
+  res.status(201);
+  res.json(question);
+});
 
 // Edit A Question
 
-
-
 // Delete A Question
-
-
 
 module.exports = router;
