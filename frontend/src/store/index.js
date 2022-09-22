be672693a018a6b36c2809992db86fcefb6449b0
kevin <kevinbarrios@ymail.com>
2022-09-21 20:55:09 -0500

    added vote reducer to index

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,12 +3,13 @@ import thunk from "redux-thunk";
 import sessionReducer from "./sessionReducer";
 import questionReducer from "./questionsReducer";
 import answerReducer from "./answersReducer";
+import voteReducer from "./votesReducer";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   questions: questionReducer,
   answers: answerReducer,
-  votes
+  votes: voteReducer
 });
 
 let enhancer;
