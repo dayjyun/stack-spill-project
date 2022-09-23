bc3e4753ec57d3f03e8f82366d57760dad88b0ba
kevin <kevinbarrios@ymail.com>
2022-09-23 03:51:37 -0500

    current progress for the night

--- frontend/src/components/QuestionsPage/AnswersComponent/Answers.js
+++ frontend/src/components/QuestionsPage/AnswersComponent/Answers.js
@@ -1,31 +1,29 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllAnswers } from "../../../store/answersReducer";
-import { getAllUsers } from "../../../store/usersReducer";
+import { getAllUsers, getUser } from "../../../store/usersReducer";
 import "./Answers.css";
 
-function AnswersComponent({ questionId }) {
+function AnswersComponent({ questionId, allUsers }) {
   const dispatch = useDispatch();
   const allAnswers = Object.values(useSelector((state) => state.answers));
-  const answers = allAnswers.filter((answer) => answer.questionId == questionId);
-  const allUsers = Object.values(useSelector((state) => state.users));
-  const userAnswer = allUsers.filter((user) => user.id == allAnswers.userId);
+  const answers = allAnswers.filter((answer) => answer?.questionId == questionId);
 
   useEffect(() => {
-    dispatch(getAllUsers());
+    dispatch(getAllUsers())
+    // dispatch(getUser(userId))
     dispatch(getAllAnswers());
   }, [dispatch]);
 
   return (
     <>
       <div>
-        <h2 id='answers-number-text'>{answers?.length} Answers</h2>
+        <h2 id="answers-number-text">{answers?.length} Answers</h2>
         {answers?.map((answer) => (
-
-            <div key={answer?.id} id="answer-details">
-              <div id="answer-body">{answer?.body}</div>
-            </div>
-
+          <div key={answer?.id} id="answer-details">
+            <div id="answer-body">{answer?.body}</div>
+            {/* by {user?.username} */}
+          </div>
         ))}
       </div>
     </>
--- frontend/src/components/QuestionsPage/QuestionComponent/QuestionComponent.js
+++ frontend/src/components/QuestionsPage/QuestionComponent/QuestionComponent.js
@@ -2,10 +2,9 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { getQuestion } from "../../../store/questionsReducer";
-import { getAllAnswers } from "../../../store/answersReducer";
-import "./QuestionComponent.css";
-import AnswersComponent from "../AnswersComponent/Answers";
 import { getUser } from "../../../store/usersReducer";
+import AnswersComponent from "../AnswersComponent/Answers";
+import "./QuestionComponent.css";
 
 function QuestionComponent() {
   const dispatch = useDispatch();
@@ -13,7 +12,7 @@ function QuestionComponent() {
   const allQuestions = Object.values(useSelector((state) => state.questions));
   const question = allQuestions.filter((question) => question.id == questionId)[0];
   const allUsers = Object.values(useSelector(state => state.users))
-  const user = allUsers.filter(user => user.id == question?.userId)[0]
+  const userQuestion = allUsers.filter(user => user.id == question?.userId)[0]
 
   useEffect(() => {
     dispatch(getQuestion(questionId));
@@ -21,13 +20,13 @@ function QuestionComponent() {
 
   return (
     <>
-      <div key={question?.id} id='question-card'>
+      <div key={question?.id} id="question-card">
         <h1 id="question-title">{question?.title}</h1>
         <h3 id="question-body">{question?.body}</h3>
-        <Link to={`/users/${user?.id}`}>By {user?.username}</Link>
+        By <Link to={`/users/${userQuestion?.id}`}>{userQuestion?.username}</Link>
       </div>
-      <div >
-        <AnswersComponent questionId={questionId}/>
+      <div>
+        <AnswersComponent questionId={questionId} allUsers={allUsers}/>
       </div>
     </>
   );
