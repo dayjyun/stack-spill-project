ec3e55ccdedd03a2050310fdf2123c4f8b52c3b7
kevin <kevinbarrios@ymail.com>
2022-09-24 03:48:07 -0500

    created a button to edit profile if currently in your profile

--- frontend/src/components/Users/UserComponent/UserComponent.js
+++ frontend/src/components/Users/UserComponent/UserComponent.js
@@ -1,22 +1,27 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
-// import { getAllQuestions } from "../../../store/questionsReducer";
 import { getUser } from "../../../store/usersReducer";
 import "./UserComponent.css";
 
 function UserComponent() {
   const dispatch = useDispatch();
   const { userId } = useParams();
+  const sessionUser = useSelector(state => state.session.user)
   const allUsers = Object.values(useSelector((state) => state.users));
   const user = allUsers.filter((user) => user.id == userId)[0];
-  // const allQuestions = Object.values(useSelector((state) => state.questions));
-  // const userQuestions = allQuestions.filter((questions) => questions.userId == userId);
+  console.log("sessionUser",sessionUser.id)
 
   useEffect(() => {
-    // dispatch(getAllQuestions());
     dispatch(getUser(userId));
   }, [dispatch, userId]);
+
+  let editButton
+
+    if (sessionUser.id == user.id) {
+      editButton = <button>Edit Profile</button>
+    }
+
 
   return (
     <>
@@ -26,13 +31,8 @@ function UserComponent() {
           <h1>{user?.username}</h1>
           <h3>{user?.firstName} {user?.lastName}</h3>
         </div>
+        {editButton}
       </div>
-      {/* {userQuestions.map((question) => (
-        <div>
-          <div>{question?.title}</div>
-          <div>{question?.body}</div>
-        </div>
-      ))} */}
       <div id="user-component-navlinks">
         <NavLink to={`/users/${userId}/questions`}>Questions</NavLink>
         <NavLink to={`/users/${userId}/answers`}>Answers</NavLink>
