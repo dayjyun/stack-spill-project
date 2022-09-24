a4b2fc9f94fb6457ee0bb7312244c0823d96be9c
kevin <kevinbarrios@ymail.com>
2022-09-24 16:30:14 -0500

    user is able to delete their questions

--- frontend/src/components/EditQuestionModal/EditQuestionForm.js
+++ frontend/src/components/EditQuestionModal/EditQuestionForm.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { deleteQuestion, editQuestion } from "../../store/questionsReducer";
 import "./EditQuestionForm.css";
 
 function EditQuestionForm({ setShowModal, questionId }) {
   const dispatch = useDispatch();
+  const history = useHistory()
   const allQuestions = Object.values(useSelector((state) => state.questions));
   const question = allQuestions.filter((question) => question.id == questionId)[0];
   const [title, setTitle] = useState(question?.title);
@@ -29,15 +31,16 @@ function EditQuestionForm({ setShowModal, questionId }) {
     setShowModal(false);
   };
 
-  const handleDeleteButton = e => {
-    e.preventDefault()
-    setShowModal(false)
-    dispatch(deleteQuestion(+questionId))
-    alert('Question deleted')
-  }
+  const handleDeleteButton = (e) => {
+    e.preventDefault();
+    setShowModal(false);
+    dispatch(deleteQuestion(+questionId));
+    alert("Question deleted");
+    history.push('/')
+  };
 
   return (
-    <form onSubmit={handelQuestionEdit} id='edit-question-form'>
+    <form onSubmit={handelQuestionEdit} id="edit-question-form">
       <label>
         Title
         <input
@@ -54,12 +57,14 @@ function EditQuestionForm({ setShowModal, questionId }) {
           onChange={(e) => setBody(e.target.value)}
         />
       </label>
-      <div id='edit-question-buttons'>
-      <button type="submit">Save</button>
-      <button onClick={handleCancelButton}>Cancel</button>
+      <div id="edit-question-buttons">
+        <button type="submit">Save</button>
+        <button onClick={handleCancelButton}>Cancel</button>
       </div>
       <div>
-        <button onClick={handleDeleteButton} id='edit-question-delete'>Delete</button>
+        <button onClick={handleDeleteButton} id="edit-question-delete">
+          Delete
+        </button>
       </div>
     </form>
   );
