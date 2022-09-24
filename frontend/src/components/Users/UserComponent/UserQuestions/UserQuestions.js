aa8a796bf7ceed0c1b95def96d25267e8884190e
kevin <kevinbarrios@ymail.com>
2022-09-23 19:53:22 -0500

    current progress with user questions css

--- frontend/src/components/Users/UserComponent/UserQuestions/UserQuestions.js
+++ frontend/src/components/Users/UserComponent/UserQuestions/UserQuestions.js
@@ -1,32 +1,40 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getAllQuestions } from '../../../../store/questionsReducer';
-import './UserQuestions.css'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useParams } from "react-router-dom";
+import { getAllQuestions } from "../../../../store/questionsReducer";
+import "./UserQuestions.css";
 
 function UserQuestions() {
-    const dispatch = useDispatch()
-    const { userId } = useParams()
-    // const allUsers = Object.values(useSelector(state => state.users))
-    // const currUser = allUsers.filter(user => user.id == userId)[0]
-    const allQuestions = Object.values(useSelector(state => state.questions))
-    const userQuestions = allQuestions.filter(questions => questions.userId == userId)
+  const dispatch = useDispatch();
+  const { userId } = useParams();
+  // const allUsers = Object.values(useSelector(state => state.users))
+  // const currUser = allUsers.filter(user => user.id == userId)[0]
+  const allQuestions = Object.values(useSelector((state) => state.questions));
+  const userQuestions = allQuestions.filter(
+    (questions) => questions.userId == userId
+  );
 
-    useEffect(() => {
-        dispatch(getAllQuestions())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getAllQuestions());
+  }, [dispatch]);
 
-    return (
-        <>
-            <h3>User Questions</h3>
-            {userQuestions.map(question => (
-                <div>
-                    <div>{question?.title}</div>
-                    <div>{question?.body}</div>
-                </div>
-            ))}
-        </>
-    )
+  return (
+    <>
+      <h3>User Questions</h3>
+      <div id='user-questions-container'>
+        {userQuestions.map((question) => (
+          <NavLink
+            key={question?.id}
+            id="user-questions-card"
+            to={{ pathname: `/questions/${question?.id}` }}
+          >
+            <div id="user-questions-title">{question?.title}</div>
+            <div id="user-questions-body">{question?.body}</div>
+          </NavLink>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default UserQuestions;
