62e4623c4d4195c84c8d0a43f5f2f1d9e2b7c168
kevin <kevinbarrios@ymail.com>
2022-09-23 19:21:13 -0500

    completed username link

--- frontend/src/components/QuestionsPage/AnswersComponent/Answers.js
+++ frontend/src/components/QuestionsPage/AnswersComponent/Answers.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { getAllAnswers } from "../../../store/answersReducer";
 import { getAllUsers, getUser } from "../../../store/usersReducer";
 import "./Answers.css";
@@ -9,11 +10,11 @@ function AnswersComponent({ questionId }) {
   const allAnswers = Object.values(useSelector((state) => state.answers));
   const answers = allAnswers.filter((answer) => answer?.questionId == questionId);
   const allUsers = Object.values(useSelector((state) => state.users));
-  const userAnswer = allUsers.filter((user) => user.id == answers?.userId);
-  
+  // const userAnswer = allUsers.filter((user) => user.id == answers?.userId);
+
   useEffect(() => {
     dispatch(getAllUsers());
-    dispatch(getUser(userAnswer));
+    // dispatch(getUser(userAnswer));
     dispatch(getAllAnswers());
   }, [dispatch]);
 
@@ -24,7 +25,10 @@ function AnswersComponent({ questionId }) {
         {answers?.map((answer) => (
           <div key={answer?.id} id="answer-details">
             <div id="answer-body">{answer?.body}</div>
-            By {allUsers?.find((user) => user?.id == answer?.userId).username}
+            By{" "}
+            <Link to={`/users/${answer?.userId}`}>
+              {allUsers?.find((user) => user?.id == answer?.userId).username}
+            </Link>
           </div>
         ))}
       </div>
