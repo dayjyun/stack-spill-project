93f714cbf01f771172f806ee7f46d1e8c9518dee
kevin <kevinbarrios@ymail.com>
2022-09-24 04:37:35 -0500

    fixed spacing

--- frontend/src/components/EditUserModal/EditUserForm.js
+++ frontend/src/components/EditUserModal/EditUserForm.js
@@ -1,88 +1,90 @@
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { editUser } from '../../store/usersReducer'
-import './EditUser.css'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { editUser } from "../../store/usersReducer";
+import "./EditUser.css";
 
 function EditUserForm() {
-  const dispatch = useDispatch()
-    const sessionUser = useSelector(state => state.session.user)
-    const [firstName, setFirstName] = useState(sessionUser?.firstName)
-    const [lastName, setLastName] = useState(sessionUser?.lastName)
-    const [email, setEmail] = useState(sessionUser?.email)
-    const [username, setUsername] = useState(sessionUser?.username)
-    const [profileImage, setProfileImage] = useState('')
-    const [password, setPassword] = useState('')
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [firstName, setFirstName] = useState(sessionUser?.firstName);
+  const [lastName, setLastName] = useState(sessionUser?.lastName);
+  const [email, setEmail] = useState(sessionUser?.email);
+  const [username, setUsername] = useState(sessionUser?.username);
+  const [profileImage, setProfileImage] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handelUserEditForm = (e) => {
-      dispatch(editUser({
+  const handelUserEditForm = (e) => {
+    dispatch(
+      editUser({
         firstName,
         lastName,
         email,
         username,
         profileImage,
         password,
-      }))
-    }
-
-    return (
-      <form onSubmit={handelUserEditForm} id='edit-user-form'>
-        <label>
-          First Name
-          <input
-            type="text"
-            value={firstName}
-            // placeholder
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label>
-          Last Name
-          <input
-            type="text"
-            value={lastName}
-            // placeholder
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            // placeholder
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            // placeholder
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="text"
-            value={password}
-            // placeholder
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label>
-          Profile Image
-          <input
-            type="text"
-            value={profileImage}
-            // placeholder
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <button type="submit">Save</button>
-      </form>
+      })
     );
+  };
+
+  return (
+    <form onSubmit={handelUserEditForm} id="edit-user-form">
+      <label>
+        First Name
+        <input
+          type="text"
+          value={firstName}
+          // placeholder
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </label>
+      <label>
+        Last Name
+        <input
+          type="text"
+          value={lastName}
+          // placeholder
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="text"
+          value={email}
+          // placeholder
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Username
+        <input
+          type="text"
+          value={username}
+          // placeholder
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="text"
+          value={password}
+          // placeholder
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Profile Image
+        <input
+          type="text"
+          value={profileImage}
+          // placeholder
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <button type="submit">Save</button>
+    </form>
+  );
 }
 
-export default EditUserForm
+export default EditUserForm;
--- frontend/src/components/EditUserModal/EditUserModal.js
+++ frontend/src/components/EditUserModal/EditUserModal.js
@@ -2,19 +2,19 @@ import { useState } from "react";
 import { Modal } from "../../context/Modal";
 import EditUserForm from "./EditUserForm";
 
-function EditUserModal () {
-    const [showModal, setShowModal] = useState(false)
+function EditUserModal() {
+  const [showModal, setShowModal] = useState(false);
 
-    return (
-        <>
-            <button onClick={() => setShowModal(true)}>Edit</button>
-            {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <EditUserForm />
-                </Modal>
-            )}
-        </>
-    )
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>Edit</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <EditUserForm />
+        </Modal>
+      )}
+    </>
+  );
 }
 
-export default EditUserModal
+export default EditUserModal;
