ee01dfbea48c4a5d3dc13cb468992951d72a2918
kevin <kevinbarrios@ymail.com>
2022-09-27 17:29:46 -0500

    current progress with profile css

--- frontend/src/components/EditComponents/EditUserModal/EditUserModal.js
+++ frontend/src/components/EditComponents/EditUserModal/EditUserModal.js
@@ -7,7 +7,7 @@ function EditUserModal() {
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Edit</button>
+      <button onClick={() => setShowModal(true)}>Edit Profile</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <EditUserForm setShowModal={setShowModal} />
