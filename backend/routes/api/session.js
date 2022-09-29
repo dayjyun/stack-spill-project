261641a6a500a6a27d59cb534c491f426d04750b
kevin <kevinbarrios@ymail.com>
2022-09-28 23:28:45 -0500

    current css progress

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -41,14 +41,14 @@ router.post("/signup", validateSignup, async (req, res) => {
   if (checkEmail) {
     let error = new Error("User already exists")
     error.status = 403;
-    error.errors = ['User with that email already exists'];
+    error.errors = ['Email already exists'];
     throw error;
   }
 
   if (checkUsername) {
     let error = new Error("User already exists")
     error.status = 403;
-    error.errors = ["User with that username already exists"];
+    error.errors = ["Username already exists"];
     throw error;
   }
 
--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -36,93 +36,91 @@ function SignupForm() {
         if (data && data.errors) setErrors(data.errors);
       });
     }
-    return setErrors([
-      "Passwords do not match",
-    ]);
+    return setErrors(["Passwords do not match"]);
   };
 
   return (
-    <div id='signup-form-container'>
-      <div id="signup-form-text">Stack Spill</div>
-      <form onSubmit={handleSubmit} id="signup-form">
-        <p>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </p>
-        <div className="signup-form-div">
-          <input
-            type="text"
-            value={firstName}
-            placeholder="Enter Your First Name"
-            className="signup-form-input"
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="signup-form-div">
-          <input
-            type="text"
-            value={lastName}
-            placeholder="Enter Your Last Name"
-            className="signup-form-input"
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="signup-form-div">
-          <input
-            type="text"
-            value={email}
-            placeholder="Enter Your Email"
-            className="signup-form-input"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="signup-form-div">
-          <input
-            type="text"
-            value={username}
-            placeholder="Enter Your Username"
-            className="signup-form-input"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="signup-form-div">
-          <input
-            type="password"
-            value={password}
-            placeholder="Enter Your Password"
-            className="signup-form-input"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="signup-form-div">
-          <input
-            type="password"
-            value={confirmPassword}
-            placeholder="Confirm Your Password"
-            className="signup-form-input"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button id="sf-buttons" type="submit">
-          Sign Up!
-        </button>
-      </form>
-      <div id="signup-form-demo">
-        <div>
+    <>
+      <div id="signup-form-container">
+        <div id="signup-form-text">Stack Spill</div>
+        <form onSubmit={handleSubmit} id="signup-form">
+          <p>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </p>
+          <div className="signup-form-div">
+            <input
+              type="text"
+              value={firstName}
+              placeholder="Enter Your First Name"
+              className="signup-form-input"
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="signup-form-div">
+            <input
+              type="text"
+              value={lastName}
+              placeholder="Enter Your Last Name"
+              className="signup-form-input"
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="signup-form-div">
+            <input
+              type="text"
+              value={email}
+              placeholder="Enter Your Email"
+              className="signup-form-input"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="signup-form-div">
+            <input
+              type="text"
+              value={username}
+              placeholder="Enter Your Username"
+              className="signup-form-input"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="signup-form-div">
+            <input
+              type="password"
+              value={password}
+              placeholder="Enter Your Password"
+              className="signup-form-input"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="signup-form-div">
+            <input
+              type="password"
+              value={confirmPassword}
+              placeholder="Confirm Your Password"
+              className="signup-form-input"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button id="sf-buttons" type="submit">
+            Sign Up
+          </button>
+        </form>
+        <div id="signup-form-demo">
           <h3>Try out the Demo User? </h3>
+          <div id="demo-user-button-signup-form">
+            <DemoUserButton />
+          </div>
         </div>
-      <div id="demo-user-button-signup-form">
-        <DemoUserButton />
       </div>
-      </div>
-    </div>
+    </>
   );
 }
 
