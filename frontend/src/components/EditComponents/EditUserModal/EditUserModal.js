51446a0372082a1d797a739756841d8129554293
kevin <kevinbarrios@ymail.com>
2022-09-29 19:47:00 -0500

    current progress with css

--- frontend/src/components/EditComponents/EditUserModal/EditUserModal.js
+++ frontend/src/components/EditComponents/EditUserModal/EditUserModal.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Modal } from "../../../context/Modal";
 import EditUserForm from "./EditUserForm";
+import './EditUserModal.css'
 
 function EditUserModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Edit Profile</button>
+      <button id='edit-user-modal-button' onClick={() => setShowModal(true)}>Edit Profile</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <EditUserForm setShowModal={setShowModal} />
