34be2776201d491ac1e0769b1d63bd8ab222fc58
kevin <kevinbarrios@ymail.com>
2022-09-30 18:50:56 -0500

    current css updates

--- frontend/src/components/QuestionsPage/QuestionComponent/QuestionVotesComponent.js
+++ frontend/src/components/QuestionsPage/QuestionComponent/QuestionVotesComponent.js
@@ -12,11 +12,11 @@ import "./QuestionComponent.css";
 function QuestionComponent() {
   const dispatch = useDispatch();
   const { questionId } = useParams();
-  const sessionUser = useSelector(state => state.session.user)
+  const sessionUser = useSelector((state) => state.session.user);
   const allQuestions = Object.values(useSelector((state) => state.questions));
   const question = allQuestions.find((question) => question.id == questionId);
-  const allUsers = Object.values(useSelector(state => state.users))
-  const currentUser = allUsers.find(user => user.id == question?.userId)
+  const allUsers = Object.values(useSelector((state) => state.users));
+  const currentUser = allUsers.find((user) => user.id == question?.userId);
 
   useEffect(() => {
     dispatch(getQuestion(questionId));
@@ -24,34 +24,39 @@ function QuestionComponent() {
 
   let userQuestionEdit;
   if (sessionUser?.id == question?.userId) {
-    userQuestionEdit = <EditQuestionModal questionId={questionId}/>
+    userQuestionEdit = <EditQuestionModal questionId={questionId} />;
   }
 
   return (
     <>
-    <div id='question-page-component'>
-      <div key={question?.id} id="question-card">
-        {/* <QuestionVotesComponent questionId={questionId}/> */}
-        <EditQuestionVote questionId={questionId}/>
-        <div>
-
-        <h1 id="question-title">{question?.title}</h1>
-        <h3 id="question-body">{question?.body}</h3>
-        {userQuestionEdit}
-        <div>
-          By{" "}
-          <Link to={`/users/${currentUser?.id}`}>
-            <img id='question-component-user-profileImage' src={currentUser?.profileImage} />
-            {currentUser?.username}
-          </Link>
-        </div>
+      <div id="question-page-component">
+        <div key={question?.id} id="question-card">
+          {/* <QuestionVotesComponent questionId={questionId}/> */}
+          <EditQuestionVote questionId={questionId} />
+          <div id='question-card-container'>
+            <h1 id="question-title">{question?.title}</h1>
+            <h3 id="question-body">{question?.body}</h3>
+            <div id="question-user-info">
+              By{" "}
+              <Link
+                id="question-component-user-link"
+                to={`/users/${currentUser?.id}`}
+              >
+                <img
+                  id="question-component-user-profileImage"
+                  src={currentUser?.profileImage}
+                />
+                {currentUser?.username}
+              </Link>
+              {userQuestionEdit}
+            </div>
           </div>
+        </div>
+        <div>
+          <AnswersComponent questionId={questionId} allUsers={allUsers} />
+        </div>
+        <CreateAnswerForm questionId={questionId} />
       </div>
-      <div>
-        <AnswersComponent questionId={questionId} allUsers={allUsers}/>
-      </div>
-      <CreateAnswerForm questionId={questionId}/>
-    </div>
     </>
   );
 }
