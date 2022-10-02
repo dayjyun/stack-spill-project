cff70dce34a84f6c35da6e42e776ca11fc9bdc7a
kevin <kevinbarrios@ymail.com>
2022-10-01 22:32:51 -0500

    currrent updates with css

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,8 +4,8 @@ import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
-import "./Navigation.css";
 import SearchBarComponent from "../SearchBar/SearchBar";
+import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
