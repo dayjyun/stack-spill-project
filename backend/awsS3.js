42df47c43b360a1f799cb7ebbeadb0ba41031964
kevin <kevinbarrios@ymail.com>
2022-10-02 22:27:24 -0500

    current progress

--- backend/awsS3.js
+++ backend/awsS3.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 // name of your bucket here
-const stackspillbucket = "aws-s3-pern-demo";
+const NAME_OF_BUCKET = "stackspillbucket";
 
 const multer = require("multer");
 
@@ -19,7 +19,7 @@ const singlePublicFileUpload = async (file) => {
   // name of the file in your S3 bucket will be the date in ms plus the extension name
   const Key = new Date().getTime().toString() + path.extname(originalname);
   const uploadParams = {
-    Bucket: stackspillbucket,
+    Bucket: NAME_OF_BUCKET,
     Key,
     Body: buffer,
     ACL: "public-read",
@@ -46,7 +46,7 @@ const singlePrivateFileUpload = async (file) => {
   // name of the file in your S3 bucket will be the date in ms plus the extension name
   const Key = new Date().getTime().toString() + path.extname(originalname);
   const uploadParams = {
-    Bucket: stackspillbucket,
+    Bucket: NAME_OF_BUCKET,
     Key,
     Body: buffer,
   };
@@ -68,7 +68,7 @@ const retrievePrivateFile = (key) => {
   let fileUrl;
   if (key) {
     fileUrl = s3.getSignedUrl("getObject", {
-      Bucket: stackspillbucket,
+      Bucket: NAME_OF_BUCKET,
       Key: key,
     });
   }
