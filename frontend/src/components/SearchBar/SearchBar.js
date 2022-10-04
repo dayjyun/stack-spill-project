80f17765cc8a435767a9054c855cbc4e840ec196
kevin <kevinbarrios@ymail.com>
2022-10-04 15:05:01 -0500

    current update with search bar

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getAllUsers } from "../../store/usersReducer";
 import "./SearchBar.css";
 
 function SearchBarComponent() {
   const dispatch = useDispatch();
   const allQuestions = Object.values(useSelector((state) => state.questions));
+  const allUsers = Object.values(useSelector(state => state.users))
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState("");
+
+  useEffect(() => {
+    dispatch(getAllUsers())
+  }, [dispatch])
+
+  let searchArray = []
 
   const questionResults = allQuestions.filter((question) => {
     return question?.title?.toLowerCase().includes(search.toLowerCase());
@@ -28,6 +36,32 @@ function SearchBarComponent() {
     );
   });
 
+  const userResults = allUsers.filter(user => {
+    return user?.username?.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const userReturns = userResults.map((user, i) => {
+    return (
+      <Link
+        to={`/users/${user?.id}`}
+        onClick={() => setSearch('')}
+        className='search-results-link'
+        key={Math.floor(Math.random() * 1000001)}
+      >
+        <div className="search-results-text">
+          <img
+            src={user?.profileImage}
+            className='search-bar-profile-pic'
+          />
+          <div className="search-bar-profile-username">{user?.username}</div>
+        </div>
+      </Link>
+    )
+  })
+
+  searchArray = [...questionReturns, ...userReturns];
+  console.log(searchArray)
+
   return (
     <>
       <div className="search-bar-container">
@@ -45,7 +79,9 @@ function SearchBarComponent() {
           </div>
         </form>
         <div className={`search-results-box ${searchResults}`}>
-          {questionReturns}
+          {/* {questionReturns} */}
+          {/* {userReturns} */}
+          {searchArray}
         </div>
       </div>
     </>
