ca79949d3075b40bbd6125940c17b0a017f6759b
kevin <kevinbarrios@ymail.com>
2022-10-04 17:17:59 -0500

    completed login and sigup button css

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import { Modal } from "../../context/Modal";
 import SignupForm from "./SignupForm";
+import './SignupFormModal.css'
 
 function SignupFormModal() {
-    const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
-    return (
-        <>
-            <button onClick={() => setShowModal(true)}>Sign Up</button>
-            {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <SignupForm />
-                </Modal>
-            )}
-        </>
-    )
+  return (
+    <>
+      <button id="signup-form-modal-button" onClick={() => setShowModal(true)}>
+        Sign Up
+      </button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <SignupForm />
+        </Modal>
+      )}
+    </>
+  );
 }
 
-export default SignupFormModal
+export default SignupFormModal;
