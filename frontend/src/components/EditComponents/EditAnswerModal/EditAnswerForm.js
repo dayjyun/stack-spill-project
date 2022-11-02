6246ffd4620e97108eb2711c0dbc5fd7cad1965a
kevin <kevinbarrios@ymail.com>
2022-11-02 16:58:00 -0500

    provided a work around for text editor issue when focused

--- frontend/src/components/EditComponents/EditAnswerModal/EditAnswerForm.js
+++ frontend/src/components/EditComponents/EditAnswerModal/EditAnswerForm.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteAnswer, editAnswer } from "../../../store/answersReducer";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 import "./EditAnswerForm.css";
 
 function EditAnswerForm({ setShowModal, answerId }) {
@@ -38,14 +40,24 @@ function EditAnswerForm({ setShowModal, answerId }) {
       <h2 id="edit-answer-form-text">Edit Your Answer</h2>
       <form onSubmit={handleAnswerEdit} id="edit-answer-form">
         <label className="edit-answer-form-label">
-          Body
-          <textarea
+          {/* Body */}
+          {/* <textarea
             type="text"
             className="edit-answer-form-input eafi-body"
             value={body}
             onChange={(e) => setBody(e.target.value)}
             required
-          />
+          /> */}
+          <div>
+            <CKEditor
+              className='eaf-text-editor'
+              editor={ClassicEditor}
+              data={body}
+              onChange={(event, editor) => {
+                setBody(editor.getData());
+              }}
+            />
+          </div>
         </label>
         <div id="edit-answer-buttons">
           <button id="eab-save" type="submit">
