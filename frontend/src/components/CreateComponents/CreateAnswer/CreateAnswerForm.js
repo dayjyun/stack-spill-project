41afa1ea4564d828fe06c3c669c66da360241e81
kevin <kevinbarrios@ymail.com>
2022-11-02 02:24:27 -0500

    created a text editor for answers component

--- frontend/src/components/CreateComponents/CreateAnswer/CreateAnswerForm.js
+++ frontend/src/components/CreateComponents/CreateAnswer/CreateAnswerForm.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createAnswer } from "../../../store/answersReducer";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 import "./CreateAnswerForm.css";
 
 function CreateAnswerForm({ questionId }) {
@@ -18,12 +20,12 @@ function CreateAnswerForm({ questionId }) {
     ).then(() => {
       e.preventDefault();
     });
-    setBody('')
+    setBody("");
   };
 
   const handleCancelButton = (e) => {
     e.preventDefault();
-    setBody('')
+    setBody("");
   };
 
   return (
@@ -32,15 +34,22 @@ function CreateAnswerForm({ questionId }) {
         <form onSubmit={handleAnswerSubmit} id="create-answer-form">
           <h3 className="create-answer-form-h3">
             Your Answer
-            <textarea
+            {/* <textarea
               className="create-answer-form-input"
               type={"text"}
               value={body}
               onChange={(e) => setBody(e.target.value)}
               required
-            />
+            /> */}
           </h3>
-          <div id='create-answer-form-buttons'>
+          <div id="body-editor">
+            <CKEditor
+              editor={ClassicEditor}
+              data={body}
+              onChange={(e, editor) => setBody(editor.getData())}
+            />
+          </div>
+          <div id="create-answer-form-buttons">
             <button id="cafb-save" type="submit">
               Submit
             </button>
